6346239436d046c137a4da5253460635cafc92bb
Alireza Sattari <alirezaraissattari@gmail.com>
2022-03-07 17:02:32 +0330

    asda

--- public/js/header.js
+++ public/js/header.js
@@ -2,4 +2,10 @@ console.log(localStorage.getItem("token"));
 if (localStorage.getItem("token")) {
     document.getElementById('register').style.display = "none";
     document.getElementById('login').style.display = "none";
+    document.getElementById('logout').style.display = "inline";
+}
+
+const clearStorage = () => {
+    localStorage.clear()
+    location.reload()
 }
